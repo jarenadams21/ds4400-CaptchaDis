1d6ad57baddc88da528e6e1c95a819c5e8cb0b48
jarenadams21 <jarenadams3@gmail.com>
2024-04-21 11:46:13 -0400

    cross fold with cv 5

--- space/audioMNIST.py
+++ space/audioMNIST.py
@@ -1,36 +1,36 @@
-from torch.utils.data import Dataset, DataLoader
+# DataSet and DataLoader
+    ## Create indexed dictionary of filenames and load one batch into memory at a time.
+    ## Use multiple workers to speed up training
+from torch.utils.data import Dataset
 from pre import AudioHandler
-import random
+
 
 file_paths = './data/'
 
+
 class AudioMNIST(Dataset):
-    def __init__(self, load_entire_filetree=False, subset_size=10000):
-        self.files = self._build_files(subset_size)
+    def __init__(self, load_entire_filetree=False):
+        self.files = self._build_files()
         self.audio_len = 48000
         self.shift_ptc = 0.4
         
-    def _build_files(self, subset_size):
+    def _build_files(self):
         files = {}
         index = 0
-        all_files = []
         for ii in range(1, 61):
             num = "0%d" % ii if ii < 10 else "%d" % ii
             for jj in range(50):
                 for kk in range(10):
-                    all_files.append([
-                        file_paths + num + "/%d_%s_%d.wav" % (kk, num, jj),
+                    files[index] = [
+                        file_paths + num + "/%d_%s_%d.wav" % (
+                            kk, num, jj),
                         kk
-                    ])
-        
-        selected_files = random.sample(all_files, subset_size)
-        for file in selected_files:
-            files[index] = file
-            index += 1
+                    ]
+                    index += 1
         return files
     
     def __len__(self):
-        return len(self.files)
+        return 30000
     
     def __getitem__(self, idx):
         signal = AudioHandler.open(self.files[idx][0])[0]
